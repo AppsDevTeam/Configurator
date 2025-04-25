0fd0ba88af9dea615666deabf54e8f3d5f9fe05d
Tomáš Kudělka <tomas@appsdevteam.com>
2025-04-25 12:38:05 +0200

    fix

--- src/Configurator.php
+++ src/Configurator.php
@@ -2,21 +2,14 @@
 
 namespace ADT;
 
+use Exception;
+use josegonzalez\Dotenv\Loader;
+use Nette\Utils\Random;
+use Tracy\Debugger;
+
 final class Configurator extends \Nette\Bootstrap\Configurator
 {
-	/**
-	 * Allowed IP addresses for debugging.
-	 */
-	private static array $debugIps = [];
-
-	/**
-	 * Developers allowed debugging.
-	 */
-	private static array $developers = [];
-
 	private static array $parameters = [];
-
-	private static bool $debugMode = false;
 
 	private string $configDirectory = '';
 
@@ -25,12 +18,12 @@ final class Configurator extends \Nette\Bootstrap\Configurator
 	{
 		$envs = [];
 		foreach (array_merge($additionalFiles, ['.env']) as $_envFile) {
-			 $envs = array_merge(
-				 (new \josegonzalez\Dotenv\Loader($envDirectory . '/' . $_envFile))
+			$envs = array_merge(
+				(new Loader($envDirectory . '/' . $_envFile))
 					->parse()
 					->skipExisting()
 					->toArray(),
-			 	$envs
+				$envs
 			);
 		}
 
@@ -48,63 +41,26 @@ final class Configurator extends \Nette\Bootstrap\Configurator
 	}
 
 
-	public function setDebugIps(array $ips): self
-	{
-		self::$debugIps = $ips;
-		return $this;
-	}
-
-
 	/**
-	 * @param string|array $publicKey
-	 * @return $this
+	 * @throws Exception
 	 */
-	public function addDeveloper($publicKey): self
-	{
-		if (self::$debugMode) {
-			throw new \Exception('Use ' . __METHOD__ . '() before setDebugMode().');
-		}
-
-		$publicKey = (array) $publicKey;
-		
-		foreach ($publicKey as $_publicKey) {
-			list($slug, $hash) = self::explodeKeySlug($_publicKey);
-			self::$developers[$slug] = $hash;
-		}
-
-		return $this;
-	}
-
-
-	public function setDebugMode(bool|string|array $value): static
-	{
-		if (!is_bool($value)) {
-			throw new \Exception('Parameter "value" must be of type boolean.');
-		}
-
-		self::$debugMode = $value;
-
-		return parent::setDebugMode(self::detectDebugMode());
-	}
-
-
 	public function addConfig(string|array|null $config): static
 	{
 		$config = (array) $config;
 
 		$config = array_filter($config);
-		
+
 		foreach ($config as $_config) {
 			$_configPath = $this->configDirectory ? $this->configDirectory . '/' . $_config . '.neon' : $_config;
 
 			if (!file_exists($_configPath)) {
-				throw new \Exception('Config file "' . $_configPath . '" does not exist.');
+				throw new Exception('Config file "' . $_configPath . '" does not exist.');
 			}
 
 			$parts = explode('/', $_config);
 			if (count($parts) === 2) {
 				if (isset(self::$parameters[$parts[0]])) {
-					throw new \Exception('You are trying to include more "' . $parts[0] . '" configs.');
+					throw new Exception('You are trying to include more "' . $parts[0] . '" configs.');
 				}
 
 				self::$parameters[$parts[0]] = $parts[1];
@@ -112,14 +68,14 @@ final class Configurator extends \Nette\Bootstrap\Configurator
 
 			parent::addConfig($_configPath);
 		}
-		
+
 		return $this;
 	}
 
 
 	public function enableTracy(?string $logDirectory = null, ?string $email = null): void
 	{
-		\Tracy\Debugger::$logSeverity = E_ALL;
+		Debugger::$logSeverity = E_ALL;
 		parent::enableTracy($logDirectory, $email);
 	}
 
@@ -133,54 +89,60 @@ final class Configurator extends \Nette\Bootstrap\Configurator
 
 	/**
 	 * Dumps public and secret key for the new developer.
-	 * @param string Slug which serves as public key comment. It's typically
+	 * @param string $slug which serves as public key comment. It's typically
 	 * developer's name. Can contain only these characters: '0-9A-Za-z./'.
 	 */
 	public static function newDeveloper(string $slug): void
 	{
-		$password = \Nette\Utils\Random::generate(32, '!-~');	// This is sufficient for strong security.
+		$password = Random::generate(32, '!-~');	// This is sufficient for strong security.
 		$secret = $slug .'@'. $password;
 		$public = $slug .'@'. password_hash($password, PASSWORD_DEFAULT);
 
 		echo "<h1>Public key</h1>";
-		\Tracy\Debugger::dump($public);
+		Debugger::dump($public);
 		echo "<h1>Secret key</h1>";
-		\Tracy\Debugger::dump($secret);
+		Debugger::dump($secret);
 		echo "<h1>Secret key (URL encoded)</h1>";
-		\Tracy\Debugger::dump(urlencode($secret));
+		Debugger::dump(urlencode($secret));
 		die();
 	}
 
 
 	/**
-	 * Detects debug mode by public and private keys stored in cookie.
+	 * @throws Exception
 	 */
-	public static function detectDebugMode($list = null): bool
+	public static function detectDebugMode(string|array|null $list = null): bool
 	{
-		if ($list) {
-			throw new \Exception('Use "setDebugIps" method instead of "list" parameter.');
-		}
-
-		if (Configurator::isCli() || !self::$developers) {
-			return self::$debugMode;
-		}
-
-		if (empty($_COOKIE[self::COOKIE_SECRET])) {
+		if (is_null($list)) {
 			return false;
 		}
 
-		$parts = static::explodeKeySlug($_COOKIE[self::COOKIE_SECRET]);
+		if (is_string($list)) {
+			$list = [$list];
+		}
+
+		if (php_sapi_name() === 'cli') {
+			throw new Exception('Not allowed for CLI.');
+		}
+
+		$developers = [];
+		foreach ($list as $_developer) {
+			list($slug, $hash) = self::explodeKeySlug($_developer);
+			$developers[$slug] = $hash;
+		}
+		if (empty($_COOKIE[self::CookieSecret])) {
+			return false;
+		}
+		$parts = self::explodeKeySlug($_COOKIE[self::CookieSecret]);
 		if (count($parts) !== 2) {
 			return false;
 		}
 		list($slug, $password) = $parts;
 
 		return
-			isset(self::$developers[$slug])
+			isset($developers[$slug])
 			&&
-			(!self::$debugIps || parent::detectDebugMode(self::$debugIps))
-			&&
-			password_verify($password, self::$developers[$slug]);
+			password_verify($password, $developers[$slug]);
 	}
 
 
@@ -195,14 +157,13 @@ final class Configurator extends \Nette\Bootstrap\Configurator
 		return self::$parameters[$name] ?? '';
 	}
 
-
 	private static function explodeKeySlug(string $key): array
 	{
 		// we have to set $limit because the password part can contain @
 		return explode('@', $key, 2);
 	}
 
-	private function convertString($str)
+	private function convertString($str): float|bool|int|string
 	{
 		if (!is_string($str)) {
 			return $str;
@@ -235,32 +196,37 @@ final class Configurator extends \Nette\Bootstrap\Configurator
 		return $str;
 	}
 
+	/**
+	 * @throws Exception
+	 */
 	public static function getConfigByUrl($urls): string
 	{
 		if (! isset($_SERVER['HTTP_HOST'])) {
-			throw new \Exception('Variable \'$_SERVER[HTTP_HOST]\' is not set.');
+			throw new Exception('Variable \'$_SERVER[HTTP_HOST]\' is not set.');
 		}
 
 		if (! isset($_SERVER['REQUEST_URI'])) {
-			throw new \Exception('Variable \'$_SERVER[REQUEST_URI]\' is not set.');
+			throw new Exception('Variable \'$_SERVER[REQUEST_URI]\' is not set.');
 		}
 
 		// Exclude server port and parameters.
 		$requestUrl = explode(':', $_SERVER['HTTP_HOST'])[0] . explode('?', $_SERVER['REQUEST_URI'])[0];
 
 		foreach ($urls as $url => $env) {
-			if (strpos($url, '^') === 0) {
+			if (str_starts_with($url, '^')) {
 				// Key in $this->urls can be regex (starts with '^').
 				if (preg_match("\x01$url\x01", $requestUrl)) {
 					return $env;
 				}
 			} else {
 				// Or just a regular string.
-				if (strpos($requestUrl . '/',  $url . '/') === 0) {
+				if (str_starts_with($requestUrl . '/', $url . '/')) {
 					// $requestUrl starts with $url and continues with a slash or ends.
 					return $env;
 				}
 			}
 		}
+
+		throw new Exception('No env was found.');
 	}
 }
